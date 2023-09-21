396141a92964cc8a4f4beaad8003983367590332
HarryNguye <harrynguyen100603@gmail.com>
2023-09-21 10:09:18 +0300

    Moi

--- src/main/java/com/example/harrybookstore/HarrybookstoreApplication.java
+++ src/main/java/com/example/harrybookstore/HarrybookstoreApplication.java
@@ -8,30 +8,35 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import com.example.harrybookstore.domain.CategoryRepository;
+
 import com.example.harrybookstore.domain.Category;
 import com.example.harrybookstore.domain.BookRepository;
 import com.example.harrybookstore.domain.Book;
-
-
 
 @SpringBootApplication
 public class HarrybookstoreApplication {
 	private static final Logger log = LoggerFactory.getLogger(HarrybookstoreApplication.class);
 
-
 	public static void main(String[] args) {
 		SpringApplication.run(HarrybookstoreApplication.class, args);
 	}
-		@Bean
-		public CommandLineRunner studentDemo(CategoryRepository repository, CategoryRepository grepository) {
-			return (args) -> {
-				log.info("save a couple of students");
-				grepository.save(new Category("Horror"));
-				grepository.save(new Category("Fantasy"));
-				grepository.save(new Category("Comedy"));
-				
-				};
 
-			}
+	@Bean
+	public CommandLineRunner studentDemo(BookRepository repository, CategoryRepository grepository) {
+		return (args) -> {
+			log.info("save a couple of students");
+			grepository.save(new Category("Horror"));
+			grepository.save(new Category("Fantasy"));
+			grepository.save(new Category("Comedy"));
+
+			repository.save(new Book("dsa", "dsa", 1,123, 1, grepository.findByName("Horror").get(0)));
+			repository.save(new Book("dsa", "dsa", 1,123, 1, grepository.findByName("Comedy").get(0)));
+
+			log.info("fetch all books");
+			for (Book book : repository.findAll()) {
+				log.info(book.toString());
 		}
 
+	};
+}
+}
--- src/main/java/com/example/harrybookstore/web/BookController.java
+++ src/main/java/com/example/harrybookstore/web/BookController.java
@@ -1,5 +1,6 @@
 package com.example.harrybookstore.web;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,25 +10,24 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.harrybookstore.domain.Book;
 import com.example.harrybookstore.domain.BookRepository;
 import com.example.harrybookstore.domain.CategoryRepository;
 import com.example.harrybookstore.domain.Category;
 
-
 @Controller
 public class BookController {
 
 	@Autowired
 	private BookRepository repository;
-	
+
 	@Autowired
 	private CategoryRepository grepository;
 
-	
-	//BOOK CONTROLLER
-	
+	// BOOK CONTROLLER
+
 	@RequestMapping(value = "/booklist", method = RequestMethod.GET)
 	public String bookList(Model model) {
 		model.addAttribute("books", repository.findAll());
@@ -35,71 +35,83 @@ public class BookController {
 		return "booklist";
 	}
 
-	//FUNCTIONALITY FOR ADDING BOOKS
-	
+	// FUNCTIONALITY FOR ADDING BOOKS
+
 	@RequestMapping(value = "/add")
 	public String addBook(Model model) {
 		model.addAttribute("book", new Book());
-    	model.addAttribute("categories", grepository.findAll());
+		model.addAttribute("categories", grepository.findAll());
 
 		return "addbook";
 	}
 
-	//FUNCTIONALITY FOR SAVING BOOKS
+	// FUNCTIONALITY FOR SAVING BOOKS
 
-	
 	@RequestMapping(value = "/save", method = RequestMethod.POST)
 	public String save(Book book) {
 		repository.save(book);
 		return "redirect:booklist";
 	}
-	
-	//FUNCTIONALITY FOR DELETING BOOKS
 
-	
+	// FUNCTIONALITY FOR DELETING BOOKS
+
 	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
 	public String deleteBook(@PathVariable("id") Long Id, Model model) {
-	repository.deleteById(Id);
-	return "redirect:../booklist";
+		repository.deleteById(Id);
+		return "redirect:../booklist";
 	}
-	
-	//FUNCTIONALITY FOR EDITING BOOKS
-	
+
+	// FUNCTIONALITY FOR EDITING BOOKS
+
 	@RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
 	public String editBook(@PathVariable("id") Long Id, Model model) {
 
 		Optional<Book> optionalBook = repository.findById(Id);
-		
+
 		if (optionalBook.isPresent()) {
 			Book book = optionalBook.get();
 			model.addAttribute("book", book);
 			model.addAttribute("categories", grepository.findAll());
-			
+
 			return "editbook";
 		} else {
 			return "redirect:/booklist";
 		}
 	}
 
-	//FUNCTIONALITY FOR UPDATING EDITED BOOKS
+	// FUNCTIONALITY FOR UPDATING EDITED BOOKS
 
 	@RequestMapping(value = "/edit/{id}", method = RequestMethod.POST)
 	public String updateBook(@ModelAttribute Book updatedBook) {
-		
+
 		Book existingBook = repository.findById(updatedBook.getId()).orElse(null);
-	    if (existingBook != null) {
-	        // Päivitä kirjan tiedot
-	        existingBook.setAuthor(updatedBook.getAuthor());
-	        existingBook.setTitle(updatedBook.getTitle());
-	        existingBook.setIsbn(updatedBook.getIsbn());
-	        existingBook.setPublicationYear(updatedBook.getPublicationYear());
-	        existingBook.setCategory(updatedBook.getCategory());
-	        
-	        repository.save(existingBook);
-	        return "redirect:/booklist";
-	        
-	    } else {
-	        return "redirect:/booklist";
-	    }
-}
+		if (existingBook != null) {
+			// Päivitä kirjan tiedot
+			existingBook.setAuthor(updatedBook.getAuthor());
+			existingBook.setTitle(updatedBook.getTitle());
+			existingBook.setIsbn(updatedBook.getIsbn());
+			existingBook.setPublicationYear(updatedBook.getPublicationYear());
+			existingBook.setCategory(updatedBook.getCategory());
+
+			repository.save(existingBook);
+			return "redirect:/booklist";
+
+		} else {
+			return "redirect:/booklist";
+		}
+
+	}
+
+	// RESTful service to get all book
+	@RequestMapping(value = "/book", method = RequestMethod.GET)
+	public @ResponseBody List<Book> bookListRest() {
+		return (List<Book>) repository.findAll();
+	}
+
+	// RESTful service to get book by id
+	@RequestMapping(value="/book/{id}", method = RequestMethod.GET)
+    public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long bookId) {	
+    	return repository.findById(bookId);
+    	
+	}
 }
